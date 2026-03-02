42dd25dfe5cd5650c2afe1b96daf2a1fe925c3cc
gtcronis <gabriel.tcronis@gmail.com>
2026-03-02 11:36:24 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Faça um algoritmo para imprimir em cada linha:
+        //o seu nome  completo, o seu registro acadêmico, o seu curso
+
+        String nome;
+        String registroAcademico;
+        String curso;
+
+        nome="Gabriel Tolcsvai de Cronis";
+        registroAcademico="1140741084";
+        curso="Análise e Desenvolvimento de Sistemas";
+        System.out.println("Nome completo"+ nome);
+        System.out.println("Registro acadêmico: "+ registroAcademico);
+        System.out.println("Curso: "+ curso);
+
+    }
+}

b9f709f258fd0bef6db4a0c270de2be5a2689337
gtcronis <gabriel.tcronis@gmail.com>
2026-03-04 10:26:25 -0300

    Add files via upload

---
+++ Aula2Ex5Media.java
@@ -0,0 +1,26 @@
+//Gabriel Tolcsvai de Cronis
+//Análise e Desenvolvimento de Sistemas/ Senac/ 1º Semestre
+//Construir um algoritmo para ler três notas de um aluno.Calcule e mostre a média aritmética do aluno.
+
+    import java.util.Scanner;
+    public class Aula2Ex5Media {
+            public static void main(String[] args) {
+                Scanner e= new Scanner(System.in);
+
+                System.out.println("Vamos calcular a sua média de notas.Digite a primeira nota: ");
+                double n1= e.nextDouble();
+
+                System.out.println("Digite a segunda nota: ");
+                double n2 = e.nextDouble();
+
+                System.out.println("Digite a terceira nota: ");
+                double n3 = e.nextDouble();
+
+                double media = (n1+n2+n3)/3;
+
+                System.out.println("Sua média é: " + media);
+
+                e.close();
+
+            }
+        }

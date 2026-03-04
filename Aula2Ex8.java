dee672507eab6533618a30bd52d6151ad0d867e2
gtcronis <gabriel.tcronis@gmail.com>
2026-03-04 11:42:56 -0300

    Add files via upload

---
+++ Aula2Ex8.java
@@ -0,0 +1,14 @@
+//Gabriel Tolcsvai de Cronis
+//Análise e Desenvolvimento de Sistemas/ Senac/ 1º Semestre
+//Construir um algoritmo para apresentar o cálculo da média das seguintes notas: 8,0;7,5;4,4 e 9.
+public class Aula2Ex8 {
+    public static void main(String[] args) {
+    double nota1=8.0;
+    double nota2=7.5;
+    double nota3=4.5;
+    double nota4=9.0;
+            double media= (nota1+nota2+nota3+nota4)/4;
+
+            System.out.printf("A média é: %.2f%n", media);
+        }
+    }

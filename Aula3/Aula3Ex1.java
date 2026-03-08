2498b4daf249968383051094480d66b0aabc9368
gtcronis <gabriel.tcronis@gmail.com>
2026-03-08 17:13:56 -0300

    Adiciona pasta Aula3 com atividades

---
+++ Aula3/Aula3Ex1.java
@@ -0,0 +1,15 @@
+//Um jogador somente irá passar para a fase 2 se atingir 1000 	pontos.
+//Gabriel Tolcsvai de Cronis. Aula 3. Exercício 1.
+
+
+public class Aula3Ex1{
+    public static void main(String[] args) {
+
+        int pontos= 1000;
+        if(pontos >= 1000) {
+        System.out.println("Parabéns! Você passou para fase 2.");
+
+
+        }
+            }
+              }

7aa47e1500ec456096e9966e7dbe4489310a462c
gtcronis <gabriel.tcronis@gmail.com>
2026-04-30 01:06:10 -0300

    Implement random number generation and display
    Added functionality to generate and display a random number between 1 and 100.

--- Aula_7 Numeros Aleatorios/Exercicio1.java
+++ Aula_7 Numeros Aleatorios/Exercicio1.java
@@ -4,12 +4,14 @@
 //Crie um programa em Java que gere um número aleatório entre 1 e 100 e exiba esse número na tela
 
 import java.util.Random;
+
 public class Exercicio1 {
     public static void main(String[] args) {
-
+        
         Random random = new Random();
-
-    int aleatorio;
-
-}
+        
+        int numeroAleatorio = random.nextInt(100) + 1;
+        
+        System.out.println(numeroAleatorio);
     }
+}

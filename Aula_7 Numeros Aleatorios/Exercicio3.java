baba123ff4b92058013d49f5e56d2f6038ac67fd
gtcronis <gabriel.tcronis@gmail.com>
2026-04-30 01:12:28 -0300

    Update Exercicio3.java

--- Aula_7 Numeros Aleatorios/Exercicio3.java
+++ Aula_7 Numeros Aleatorios/Exercicio3.java
@@ -10,8 +10,34 @@
 //A média dos valores e Quantos números são pares
 //RESTRIÇÃO: Não utilizar vetores(arrays).Utilize apenas variáveis simples e estruturadas de repetição
 
+import java.util.Random;
+
 public class Exercicio3 {
-
-
-
+    public static void main(String[] args) {
+      
+        Random gerador = new Random();
+        
+      
+        double somaTotal = 0.0;
+        int contadorPares = 0;
+        
+       
+            for (int i = 0; i < 10; i++) {
+          
+            int numeroSorteado = gerador.nextInt(20) + 1;
+            System.out.println("Número da vez: " + numeroSorteado);
+            
+        somaTotal = somaTotal + numeroSorteado;
+                     
+            if (numeroSorteado % 2 == 0) {
+                contadorPares = contadorPares + 1;
+            }
+        }
+        
+          double media = somaTotal / 10;
+        
+        System.out.println("Soma final: " + somaTotal);
+        System.out.println("média:  " + media);
+        System.out.println("Total de números pares: " + contadorPares);
+    }
 }
